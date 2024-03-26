358bafedd9024158dd0ba5c1c920c9bc8ba94be1
martin <martingerwallner@gmx.de>
2024-03-26 22:06:16 +0100

    Add keymap

--- plugin/terminal-split.lua
+++ plugin/terminal-split.lua
@@ -1,20 +1,20 @@
 local M = {}
 
-function M.split_terminal()
-  if vim.fn.bufexists(M.buffer) == 0 then
-    M.buffer = vim.api.nvim_create_buf(true, false)
-    vim.cmd("sb" .. M.buffer)
-    vim.cmd("terminal")
-  else
-    vim.cmd("sb " .. M.buffer)
-  end
-  vim.cmd("resize 10")
-end
-
 function M.setup()
   M.buffer = vim.api.nvim_create_buf(true, false)
   vim.cmd("b" .. M.buffer)
   vim.cmd("terminal")
+
+  vim.keymap.set("n", "<Leader>nt", function()
+    if vim.fn.bufexists(M.buffer) == 0 then
+      M.buffer = vim.api.nvim_create_buf(true, false)
+      vim.cmd("sb" .. M.buffer)
+      vim.cmd("terminal")
+    else
+      vim.cmd("sb " .. M.buffer)
+    end
+    vim.cmd("resize 10")
+  end)
 end
 
 return M
